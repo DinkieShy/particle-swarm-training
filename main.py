742e477cd1c036be23abcf75ed32687fd298b370
Grey <david.churchill72@gmail.com>
2023-06-26 17:26:22 +0100

    spawn process to run network and collect final training loss. Added gitignore for env/

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import numpy as np
+import subprocess
+from multiprocessing import Pool, TimeoutError
+from tqdm.contrib.concurrent import process_map
+from tqdm import tqdm
 
 class ParticleSwarm():
-    def __init__(self, dim):
-        self.count = 3
+    def __init__(self, dim, count=3):
+        self.count = count
         self.dimensions = dim # 2d dict of [dimension][0|1] for lower/upper bound
         '''
         Learning rate
@@ -24,25 +28,52 @@ class Particle():
 
         for dim in dimensions:
             if isinstance(dimensions[dim][0], (int)):
-                self.position[dim] = np.random.randint(dimensions[dim][0], dimensions[dim][1])
+                self.position[dim] = str(np.random.randint(dimensions[dim][0], dimensions[dim][1]))
             else:
-                self.position[dim] = np.random.uniform(dimensions[dim][0], dimensions[dim][1])
+                self.position[dim] = str(np.random.uniform(dimensions[dim][0], dimensions[dim][1]))
 
+def runParticle(particle, progBar = None):
+    args = ["python", "runNetwork.py"]
+    for (key, value) in particle.position.items():
+        args.append(key)
+        args.append(value)
+    
+    output = subprocess.run(args, capture_output=True)
+    if progBar is not None:
+        progBar.update()
+    return float(output.stdout)
 
 def main():
+    MAX_THREADS = 5
+
     dimensions = {
-        "learningRate0": (0.0005, 0.002),
-        "learningRate0Epochs": (2, 10),
-        "learningRate1": (0.00001, 0.0005),
-        "momentum": (0.75, 0.95),
-        "weightDecay": (0.00001, 0.0001)
+        "--lr": (0.0005, 0.002),
+        "--lr-drop": (2, 10),
+        "--lr2": (0.00001, 0.0005),
+        "--momentum": (0.75, 0.95),
+        "--decay": (0.00001, 0.0001),
+        "--batch-size": (16, 128)
     }
 
-    swarm = ParticleSwarm(dimensions)
+    swarm = ParticleSwarm(dimensions, count=30)
 
     swarm.initialiseSwarm()
+    output = np.array([], dtype=np.float32)
 
-    print(swarm.particles[0].position["learningRate0Epochs"])
+    with Pool(processes=MAX_THREADS) as pool:
+        try:
+            pbar = tqdm(total=swarm.count, miniters=1, mininterval=1, maxinterval=1)
+            for run in pool.map(runParticle, swarm.particles):
+                output = np.append(output, [run])
+                pbar.update()
+        except TimeoutError:
+            assert False, "Timeout error"
+
+    # output = process_map(runParticle, swarm.particles, max_workers=MAX_THREADS, total=swarm.count, monitor_interval=1)
+
+    print(output)
+    print(f'max: {output.max()}, min: {output.min()}, mean: {output.mean()}, median: {np.median(output)}')
+
 
 if __name__ == "__main__":
     main()
--- runNetwork.py
+++ runNetwork.py
@@ -1,5 +1,109 @@
 import torch
-import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from tqdm import tqdm
 
+import argparse
 from sys import argv
 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+    
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    loss = 0
+    # for batch_idx, (data, target) in (progressBar := tqdm(enumerate(train_loader), desc = "Training...")):
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        # if batch_idx % args.log_interval == 0:
+        #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+        #         epoch, batch_idx * len(data), len(train_loader.dataset),
+        #         100. * batch_idx / len(train_loader), loss.item()))
+        #     if args.dry_run:
+        #         break
+        
+    return loss
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--epochs', type=int, default=15, metavar='N',
+                    help='number of epochs to train (default: 14)')
+parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                    help='learning rate (default: 1.0)')
+parser.add_argument('--lr2', type=float, default=0.0001, metavar='LR2',
+                    help="second learning rate to use")
+parser.add_argument('--lr-drop', type=int, default=10, metavar="LRD",
+                    help="how many epochs to wait before dropping learning rate")
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help="momentum value for SGD")
+parser.add_argument('--decay', type=float, default=0.00001, metavar='WD',
+                    help="weight decay value")
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+args = parser.parse_args()
+use_cuda = torch.cuda.is_available()
+
+torch.manual_seed(args.seed)
+
+if use_cuda:
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+train_kwargs = {'batch_size': args.batch_size}
+if use_cuda:
+    cuda_kwargs = {'num_workers': 1,
+                    'pin_memory': True,
+                    'shuffle': True}
+    train_kwargs.update(cuda_kwargs)
+
+transform=transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+    ])
+dataset1 = datasets.MNIST('../data', train=True, download=True,
+                    transform=transform)
+train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+
+model = Net().to(device)
+optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+# for epoch in range(1, args.epochs + 1):
+for epoch in range(1, 15 + 1):
+    loss = train(args, model, device, train_loader, optimizer, epoch)
+    if epoch == args.lr_drop:
+        for i in optimizer.param_groups:
+            i['lr'] = args.lr2
+
+print(loss.item())
